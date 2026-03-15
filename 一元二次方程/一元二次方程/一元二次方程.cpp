60d446185531fd0ff46f57a8e982a43e824f730b
qwwqq123 <zzh061221@outlook.com>
2026-03-15 22:41:31 +0800

    Add files via upload

---
+++ 一元二次方程/一元二次方程/一元二次方程.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    scanf("%f %f %f", &a, &b, &c);
+    float d = b * b - 4 * a * c;
+    if (d < 0)
+    {
+        printf("%.2f+%.2fi %.2f-%.2fi", -b / (2 * a), sqrt(-d) / (2 * a), -b / (2 * a), sqrt(-d) / (2 * a));
+    }
+    else if (d == 0)
+    {
+        printf("%.2f %.2f", -b / (2 * a), -b / (2 * a));
+    }
+    else
+    {
+        printf("%.2f %.2f", (-b + sqrt(d)) / (2 * a), (-b - sqrt(d)) / (2 * a));
+    }
+    return 0;
+}
